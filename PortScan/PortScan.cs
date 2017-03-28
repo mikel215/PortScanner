1965194a4ad88254e0f994c72c29ed703b49015c
Mik <Mik@MIK-PC>
2017-03-27 19:54:51 -0700

    Add project files.

---
+++ PortScan/PortScan.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace dfnPortScanner
+{
+    public delegate void SinglePortScanFinishEventHandler(object sender, PortScannerEventArgs e);
+
+    public class PortScan
+    {
+        private IPAddress address;
+        private int[] ports;
+
+        public IPAddress Address
+        {
+            get { return address; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(Address));
+
+                if (value.Equals(IPAddress.Any)
+                 || value.Equals(IPAddress.IPv6Any)
+                 || value.Equals(IPAddress.None)
+                 || value.Equals(IPAddress.IPv6None))
+                    throw new ArgumentException(@"IP Address cannot be any.", nameof(Address));
+
+                address = value;
+            }
+        }
+
+        public int[] Ports
+        {
+            get { return ports; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                if (value.Length == 0)
+                    throw new ArgumentException(@"Cannot be empty.", nameof(value));
+
+                ports = value;
+            }
+        }
+
+        public event SinglePortScanFinishEventHandler OnSinglePortScanFinish;
+
+        public PortScan(IPAddress address, params int[] ports)
+        {
+            Address = address;
+            Ports = ports;
+        }
+
+        public virtual PortScanResult[] Scan()
+        {
+            return InternalScan();
+        }
+
+        public Poe
+
+    }
+
+    // PortScanner event
+    public class PortScannerEventArgs : EventArgs
+    {
+        // PortScanResult Property
+        public PortScanResult PortScanResult { get; set; }
+
+        // PortScannerEventArgs method
+        public PortScannerEventArgs(PortScanResult result)
+        {
+            // 
+            PortScanResult = result;
+        }
+    }
+
+    // PortScanResult class
+    public class PortScanResult
+    {
+        //  Port property   
+        public int Port { get; set; }
+
+        // Open property
+        public bool Open { get; set; }
+    }
+}
---
+++ dfnPortScanner/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Forms;
+using dfnPortScanner.Properties;
+
+namespace dfnPortScanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int[] ports;
+        private IPAddress address;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void scanButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(!ParseAddress())
+            {
+                MessageBox.Show(@"Failed to parse address");
+                return;
+            }
+            if (!ParsePorts())
+            {
+                MessageBox.Show(@"Failed to parse port(s)");
+                return;
+            }
+
+            lvOutput.Items.Clear();
+
+            var portScanner = new dfnPortScanner.PortScanner
+        }
+
+        
+
+        private bool ParsePorts()
+        {
+            IEnumerable<int> portRange = Enumerable.Range(Convert.ToInt32(fromBox.Text.Trim()), Convert.ToInt32(toBox.Text.Trim()));
+
+            var tempPorts = portRange.ToArray();
+
+            ports = new int[tempPorts.Length];
+
+            for (int i = 0; i < tempPorts.Length; i++)
+            {
+                var portResult = ConvertPort(tempPorts[i]);
+                if (!portResult.HasValue)
+                {
+                    return false;
+                }
+
+                ports[i] = portResult.Value;
+
+            }
+
+            return true;
+
+        }
+
+        private bool ParseAddress()
+        {
+            var rawAddress = ipBox.Text.Trim();
+
+            return IPAddress.TryParse(rawAddress, out address);
+        }
+
+        private static int? ConvertPort(string raw)
+        {
+            int result;
+            if (!int.TryParse(raw, out result))
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        
+    }
+}
