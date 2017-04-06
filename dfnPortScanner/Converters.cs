fcf356b42a84a25e61c1fdcaa755ec15368e7f63
Mikel A <gameloft1@hotmail.com>
2017-04-06 16:09:59 -0700

    2

---
+++ dfnPortScanner/Converters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace dfnPortScanner
+{
+	public class StatusConverter : IValueConverter
+	{
+        public object Convert(object value, Type targetType, object param, CultureInfo culture)
+        {
+            string portStatus = ((string)value).ToLower();
+            if(portStatus == "closed")
+            {
+                return "Red";
+            }
+            else
+            {
+                return "Green";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object param, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+	}
+}
